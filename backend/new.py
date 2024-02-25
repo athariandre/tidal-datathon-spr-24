2279420e974434dc6124b56b11e090642a43c8a0
imranhemani <i.hemani08@gmail.com>
2024-02-25 01:38:09 -0600

    hi

--- backend/new.py
+++ backend/new.py
@@ -83,6 +83,157 @@ class Path:
 
         return point_list
     
+    def pointWeatherData(point_list):
+
+
+
+        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
+
+        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
+
+        url = "https://api.open-meteo.com/v1/forecast"
+
+
+        latstr = ""
+        lonstr = ""
+        
+        for datapoint in point_list:
+            lat = datapoint[0]
+            lon = datapoint[1]
+            latstr+=str(lat)+","
+            lonstr+=str(lon)+","
+
+        latstr = latstr[:-1]
+        lonstr = lonstr[:-1]
+
+        params = {
+            "latitude": latstr,
+            "longitude": lonstr,
+            "hourly": ["temperature_2m", "relative_humidity_2m", "precipitation", "rain", "snowfall", "cloud_cover_high", "wind_speed_10m", "soil_temperature_0cm"],
+            "temperature_unit": "fahrenheit",
+            "timezone": "America/Chicago",
+            "start_date": "2024-02-27",
+            "end_date": "2024-02-27"
+        }
+
+
+        responses = openmeteo.weather_api(url, params=params)
+
+        # Process first location. Add a for-loop for multiple locations or weather models
+        response = responses[0]
+        print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+        print(f"Elevation {response.Elevation()} m asl")
+        print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+        print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+        # Process hourly data. The order of variables needs to be the same as requested.
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+        hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+        hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
+        hourly_rain = hourly.Variables(3).ValuesAsNumpy()
+        hourly_snowfall = hourly.Variables(4).ValuesAsNumpy()
+        hourly_cloud_cover_high = hourly.Variables(5).ValuesAsNumpy()
+        hourly_wind_speed_10m = hourly.Variables(6).ValuesAsNumpy()
+        hourly_soil_temperature_0cm = hourly.Variables(7).ValuesAsNumpy()
+
+        hourly_data = {"date": pd.date_range(
+            start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+            end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+            freq = pd.Timedelta(seconds = hourly.Interval()),
+            inclusive = "left"
+        )}
+        hourly_data["temperature_2m"] = hourly_temperature_2m
+        hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+        hourly_data["precipitation"] = hourly_precipitation
+        hourly_data["rain"] = hourly_rain
+        hourly_data["snowfall"] = hourly_snowfall
+        hourly_data["cloud_cover_high"] = hourly_cloud_cover_high
+        hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+        hourly_data["soil_temperature_0cm"] = hourly_soil_temperature_0cm
+
+        hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+
+        hourly_dataframe["sleet"] = 0
+        
+
+
+
+        #Define conditions for sleet
+        sleet_conditions = (
+            (hourly_dataframe['temperature_2m'] <= 32) &
+            (hourly_dataframe['snowfall'] == 0) &
+            (hourly_dataframe["precipitation"])> 0.0     
+        )
+        hourly_dataframe['sleet'] = 0.0                     
+        hourly_dataframe.loc[sleet_conditions, 'sleet'] = 1.0  
+
+
+        #turn everything into boolean 0 and 1
+        for i,row in hourly_dataframe.iterrows():
+            temp = row["relative_humidity_2m"]
+            if temp > 83.0:
+                hourly_dataframe.at[i,"relative_humidity_2m"] = 1.0
+            else:
+                hourly_dataframe.at[i,"relative_humidity_2m"] = 0.0
+
+
+        for i,row in hourly_dataframe.iterrows():
+            temp = row["rain"]
+            if temp > 0.0:
+                hourly_dataframe.at[i,"rain"] = 1.0
+            else:
+                hourly_dataframe.at[i,"rain"] = 0.0
+
+
+        for i,row in hourly_dataframe.iterrows():
+            temp = row["snowfall"]
+            if temp > 0.0:
+                hourly_dataframe.at[i,"snowfall"] = 1.0
+            else:
+                hourly_dataframe.at[i,"snowfall"] = 0.0
+
+        for i,row in hourly_dataframe.iterrows():
+            temp = row["wind_speed_10m"]
+            if temp >= 10.0:
+                hourly_dataframe.at[i,"wind_speed_10m"] = 1.0
+            else:
+                hourly_dataframe.at[i,"wind_speed_10m"] = 0.0
+
+        #Define conditions for clear
+        clear_conditions = (
+            (hourly_dataframe['snowfall'] == 0.0) &
+            (hourly_dataframe['rain'] == 0.0) &
+            (hourly_dataframe["wind_speed_10m"] == 0.0) & 
+            (hourly_dataframe["relative_humidity_2m"] == 0.0) &
+            (hourly_dataframe["sleet"] == 0.0)
+        )
+        hourly_dataframe['clear'] = 0.0                     
+        hourly_dataframe.loc[clear_conditions, 'clear'] = 1.0
+
+
+        drop_columns = ['temperature_2m','precipitation','cloud_cover_high','soil_temperature_0cm']
+        hourly_dataframe= hourly_dataframe.drop(columns=drop_columns)
+
+        hourly_dataframe['date'] = hourly_dataframe['date'].astype(str)
+            
+        for i,row in hourly_dataframe.iterrows():
+            temp = row["date"]
+            temp = temp[:-12]
+            hourly_dataframe.at[i,"date"] = temp
+            
+        hourly_dataframe['soil'] = 0.0
+
+
+        hourly_dataframe = hourly_dataframe.rename(columns={'relative_humidity_2m': 'fog', 'wind_speed_10m': 'wind','snowfall': 'snow'})
+
+        column_order = ['date', 'clear','soil', 'fog', 'rain', 'wind', 'sleet','snow']
+        hourly_dataframe = hourly_dataframe[column_order]
 
     
 
